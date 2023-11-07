27e9bb234975ad11b849398f74653737ec853930
nickshefer <nicitashefer@yandex.ru>
2023-11-07 23:02:24 +0700

    catalog

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+// burger
 const burger = document.querySelector('.header-burger');
 const menu = document.querySelector('.header-nav');
-const links = document.querySelectorAll('a');
-// burger
-burger.addEventListener('click', () => {
-	burger.classList.toggle('active');
-	menu.classList.toggle('active');
-	document.body.classList.toggle('hidden');
+document.addEventListener('click', e => {
+	if (burger === e.target) {
+		menu.classList.toggle('active');
+		document.body.classList.toggle('hidden');
+	} else if (
+		e.target.closest('.header-nav') ||
+		!menu.classList.contains('active')
+	) {
+		return;
+	} else {
+		menu.classList.remove('active');
+		document.body.classList.remove('hidden');
+	}
 });
+
 // links
+const links = document.querySelectorAll('a');
 links.forEach(link => {
 	link.addEventListener('click', e => {
 		if (link.textContent != 'Whatsapp') {
@@ -23,12 +33,48 @@ links.forEach(link => {
 		burger.classList.contains('active') ? burger.click() : null;
 	});
 });
+
 // hero-title
-const swiper = new Swiper('.swiper', {
+const swiper1 = new Swiper('.swiper1', {
 	direction: 'vertical',
 	loop: true,
 	autoplay: {
 		delay: 1500
 	}
-	// Navigation arrows
+});
+
+// hero-btn
+const heroBtn = document.querySelector('.hero-btn');
+heroBtn.addEventListener('click', () => {
+	let id = heroBtn.getAttribute('href');
+	let top = document.querySelector(id).offsetTop;
+	window.scrollTo({
+		top: top,
+		behavior: 'smooth'
+	});
+});
+
+// catalog slider
+const swiper2 = new Swiper('.swiper2', {
+	spaceBetween: 10,
+	navigation: {
+		nextEl: '.swiper-btn-next',
+		prevEl: '.swiper-btn-prev'
+	}
+});
+
+// popup
+const popup = document.querySelector('.popup');
+const catalogBtn = document.querySelector('.catalog-btn');
+const closeBtn = document.querySelector('.popup-header_close');
+function showPopup() {
+	popup.classList.add('active');
+	document.body.classList.add('hidden');
+}
+catalogBtn.addEventListener('click', showPopup);
+document.addEventListener('click', e => {
+	if (e.target == closeBtn) {
+		popup.classList.remove('active');
+		document.body.classList.remove('hidden');
+	}
 });
